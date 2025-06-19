3db912ab0f50d73f33189d67a91a8a18c6305324
Ananth Kannan <45868647+ananthvamsi555@users.noreply.github.com>
2025-06-19 12:13:40 +0530

    Update Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,10 +1,10 @@
 package com.mycompany.app;
-public class Hello {
-  
-  public static String GREETING = "Hello world!";
-  
-  public static void main(String []args) {
-    
-  }  
-}
 
+public class Hello {
+
+    public static String GREETING = "Hello world!";
+
+    public static void main(String[] args) {
+        System.out.println(GREETING);
+    }
+}
